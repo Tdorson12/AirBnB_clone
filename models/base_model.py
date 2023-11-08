23322a4f05de2eb82509d3f67f81bd32da4d71c8
AcheampongAndy <acheampongandrews1999@gmail.com>
2023-11-08 01:11:35 -0800

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -5,41 +5,59 @@ from datetime import datetime
 
 
 class BaseModel:
-    """" This defines common attributes and methods for other classes """
+    """"
+        This defines common attributes and methods for other classes
+    """
     def __init__(self, *args, **kwargs):
-        """ Initialize BaseModel instance. """
+        """
+            Initialize BaseModel instance.
+        """
         if kwargs:
-            """ if kwargs is not empty set attributes from kwargs """
+            """
+                if kwargs is not empty set attributes from kwargs
+            """
             for key, value in kwargs.items():
                 if key != "__class__":
                     setattr(self, key, value)
                     if key in ["created_at", "updated_at"]:
-                        """ convert created_at and updated_at to datetime """
+                        """
+                            convert created_at and updated_at to datetime
+                        """
                         val = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                         setattr(self, key, val)
         else:
-            """ if kwargs is empty generate the foll """
+            """
+               if kwargs is empty generate the foll
+            """
             self.id = str(uuid.uuid4())
             self.created_at = datetime.utcnow()
-            """ Set update_at with the value of created_at """
-            self.updated_at = self.created_at
+            """
+                Set update_at with the value of created_at
+            """
+            self.updated_at = datetime.utcnow()
 
     def __str__(self):
-        """ Print [<class name>] (<self.id>) <self.__dict__> to the screen """
+        """
+            Print [<class name>] (<self.id>) <self.__dict__> to the screen
+        """
         return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """ Update the updated_at to the current datetime """
+        """
+        Update the updated_at to the current datetime
+        """
         updated_at = datetime.utcnow()
 
     def to_dict(self):
-        """ A dictionary containing all keys and values of the instance """
-        return {
-            "id": self.id,
-            "created_at": self.created_at.isoformat(),
-            "updated_at": self.updated_at.isoformat(),
-            "__class__": type(self).__name__
-            }
+        """
+        Return dictionary representation of BaseModel class.
+        """
+        self.__dict__["id"] = self.id
+        self.__dict__["created_at"] = self.created_at.isoformat()
+        self.__dict__["updated_at"] = self.updated_at.isoformat()
+        self.__dict__["__class__"] = self.__class__.__name__
+
+        return dict(self.__dict__)
 
 
 if __name__ == "__main__":
