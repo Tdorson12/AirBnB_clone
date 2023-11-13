d6a1b284372e13c654b3af3dfc0fe04f8e208dfb
AcheampongAndy <acheampongandrews1999@gmail.com>
2023-11-13 03:04:02 -0800

    changes

--- models/user.py
+++ models/user.py
@@ -4,9 +4,27 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """User class inherits from BaseModel"""
+    """
+    User class inherits from BaseModel
+    """
 
-    email: str = ""
-    password: str = ""
-    first_name: str = ""
-    last_name: str = ""
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize User instance.
+        """
+        super().__init__(*args, **kwargs)
+        self.email = ""
+        self.password = ""
+        self.first_name = ""
+        self.last_name = ""
+
+    def to_dict(self):
+        """
+        Return dictionary representation of User class.
+        """
+        user_dict = super().to_dict()
+        user_dict['email'] = self.email
+        user_dict['password'] = self.password
+        user_dict['first_name'] = self.first_name
+        user_dict['last_name'] = self.last_name
+        return user_dict
